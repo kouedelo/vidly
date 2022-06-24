5b8e92cfee6cd7c95067d817256ed7c57c94f1e5
Kouede Loic <kouedelo@gmail.com>
2022-06-24 11:56:33 -0400

    Implemented delete , update and save movie features

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,10 +3,28 @@ import { apiEndpoint } from "../config.json";
 
 const moviesEndpoint = apiEndpoint + "/movies";
 
+function movieUrl(movieId) {
+  return `${moviesEndpoint}/${movieId}`;
+}
+
 export function getMovies() {
   return http.get(moviesEndpoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
+
 export function deleteMovie(movieId) {
-  return http.delete(moviesEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
+}
+
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+
+  return http.post(moviesEndpoint, movie);
 }
